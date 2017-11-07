3e09a401668f12a6932e29369468656a502c33eb
Akshay Easwaran <akeaswaran@me.com>
2017-11-07 13:58:06 -0500

    trying to fix updater loop crashes

--- harbaughsim16/AppDelegate.m
+++ harbaughsim16/AppDelegate.m
@@ -144,6 +144,7 @@
                 _league.userTeam = nil;
                 dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
                     [[NSNotificationCenter defaultCenter] postNotificationName:@"noSaveFile" object:nil];
+                    [self displayIntro];
                 });
             }
         }]];
