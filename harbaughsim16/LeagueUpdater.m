b361de437e778f2fdff0e30bd10833be70ed4dee
Akshay Easwaran <akeaswaran@me.com>
2019-02-20 00:54:03 -0500

    adding update logic

--- harbaughsim16/LeagueUpdater.m
+++ harbaughsim16/LeagueUpdater.m
@@ -384,6 +384,7 @@
         }
         
         if ([[self class] needsUpdateFromVersion:oldLigue.leagueVersion toVersion:@"3.0"]) {
+            __block float prgs = 0.0;
             oldLigue.isCareerMode = NO;
             oldLigue.coachList = [NSMutableArray array];
             oldLigue.coachStarList = [NSMutableArray array];
@@ -425,8 +426,174 @@
                 [oldLigue getCoachAwardStr];
             }
             
+            // add defensive stats to all games
+            for (Team *t in oldLigue.teamList) {
+                for (Game *g in t.gameSchedule) {
+                    g.HomeCB1Stats = [NSMutableArray array];
+                    g.HomeCB2Stats = [NSMutableArray array];
+                    g.HomeCB3Stats = [NSMutableArray array];
+                    g.AwayCB1Stats = [NSMutableArray array];
+                    g.AwayCB2Stats = [NSMutableArray array];
+                    g.AwayCB3Stats = [NSMutableArray array];
+                    
+                    g.HomeLB1Stats = [NSMutableArray array];
+                    g.HomeLB2Stats = [NSMutableArray array];
+                    g.HomeLB3Stats = [NSMutableArray array];
+                    g.AwayLB1Stats = [NSMutableArray array];
+                    g.AwayLB2Stats = [NSMutableArray array];
+                    g.AwayLB3Stats = [NSMutableArray array];
+                    
+                    g.HomeDL1Stats = [NSMutableArray array];
+                    g.HomeDL2Stats = [NSMutableArray array];
+                    g.HomeDL3Stats = [NSMutableArray array];
+                    g.HomeDL4Stats = [NSMutableArray array];
+                    g.AwayDL1Stats = [NSMutableArray array];
+                    g.AwayDL2Stats = [NSMutableArray array];
+                    g.AwayDL3Stats = [NSMutableArray array];
+                    g.AwayDL4Stats = [NSMutableArray array];
+                    g.HomeSStats = [NSMutableArray array];
+                    g.AwaySStats = [NSMutableArray array];
+                    
+                    for (int i = 0; i < 5; i++) {
+                        [g.HomeDL1Stats addObject:@(0)];
+                        [g.AwayDL1Stats addObject:@(0)];
+                        
+                        [g.HomeDL2Stats addObject:@(0)];
+                        [g.AwayDL2Stats addObject:@(0)];
+                        
+                        [g.HomeDL3Stats addObject:@(0)];
+                        [g.AwayDL3Stats addObject:@(0)];
+                        
+                        [g.HomeDL4Stats addObject:@(0)];
+                        [g.AwayDL4Stats addObject:@(0)];
+                        
+                        [g.HomeLB1Stats addObject:@(0)];
+                        [g.AwayLB1Stats addObject:@(0)];
+                        
+                        [g.HomeLB2Stats addObject:@(0)];
+                        [g.AwayLB2Stats addObject:@(0)];
+                        
+                        [g.HomeLB3Stats addObject:@(0)];
+                        [g.AwayLB3Stats addObject:@(0)];
+                        
+                        [g.HomeCB1Stats addObject:@(0)];
+                        [g.AwayCB1Stats addObject:@(0)];
+                        
+                        [g.HomeCB2Stats addObject:@(0)];
+                        [g.AwayCB2Stats addObject:@(0)];
+                        
+                        [g.HomeCB3Stats addObject:@(0)];
+                        [g.AwayCB3Stats addObject:@(0)];
+                        
+                        [g.HomeSStats addObject:@(0)];
+                        [g.AwaySStats addObject:@(0)];
+                    }
+                }
+            }
+            
+            // if NCG, bowls, semis are not null, add TEStats and TE and 4 more QB stats
+            NSMutableArray *leagueGames;
+            if (oldLigue.bowlGames.count > 0) {
+                leagueGames = [NSMutableArray arrayWithArray:oldLigue.bowlGames];
+            }
+            
+            if (oldLigue.ncg != nil) {
+                [leagueGames addObject:oldLigue.ncg];
+            }
+            
+            if (oldLigue.semiG14 != nil) {
+                [leagueGames addObject:oldLigue.semiG14];
+            }
+            
+            if (oldLigue.semiG23 != nil) {
+                [leagueGames addObject:oldLigue.semiG23];
+            }
+            
+            for (Game *g in leagueGames) {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    prgs += (0.30 / leagueGames.count);
+                    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.75 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                        updatingBlock(prgs, @"Updating league structure for version 3...");
+                    });
+                });
+                if (g != nil) {
+                    g.HomeCB1Stats = [NSMutableArray array];
+                    g.HomeCB2Stats = [NSMutableArray array];
+                    g.HomeCB3Stats = [NSMutableArray array];
+                    g.AwayCB1Stats = [NSMutableArray array];
+                    g.AwayCB2Stats = [NSMutableArray array];
+                    g.AwayCB3Stats = [NSMutableArray array];
+                    
+                    g.HomeLB1Stats = [NSMutableArray array];
+                    g.HomeLB2Stats = [NSMutableArray array];
+                    g.HomeLB3Stats = [NSMutableArray array];
+                    g.AwayLB1Stats = [NSMutableArray array];
+                    g.AwayLB2Stats = [NSMutableArray array];
+                    g.AwayLB3Stats = [NSMutableArray array];
+                    
+                    g.HomeDL1Stats = [NSMutableArray array];
+                    g.HomeDL2Stats = [NSMutableArray array];
+                    g.HomeDL3Stats = [NSMutableArray array];
+                    g.HomeDL4Stats = [NSMutableArray array];
+                    g.AwayDL1Stats = [NSMutableArray array];
+                    g.AwayDL2Stats = [NSMutableArray array];
+                    g.AwayDL3Stats = [NSMutableArray array];
+                    g.AwayDL4Stats = [NSMutableArray array];
+                    g.HomeSStats = [NSMutableArray array];
+                    g.AwaySStats = [NSMutableArray array];
+                    
+                    for (int i = 0; i < 5; i++) {
+                        [g.HomeDL1Stats addObject:@(0)];
+                        [g.AwayDL1Stats addObject:@(0)];
+                        
+                        [g.HomeDL2Stats addObject:@(0)];
+                        [g.AwayDL2Stats addObject:@(0)];
+                        
+                        [g.HomeDL3Stats addObject:@(0)];
+                        [g.AwayDL3Stats addObject:@(0)];
+                        
+                        [g.HomeDL4Stats addObject:@(0)];
+                        [g.AwayDL4Stats addObject:@(0)];
+                        
+                        [g.HomeLB1Stats addObject:@(0)];
+                        [g.AwayLB1Stats addObject:@(0)];
+                        
+                        [g.HomeLB2Stats addObject:@(0)];
+                        [g.AwayLB2Stats addObject:@(0)];
+                        
+                        [g.HomeLB3Stats addObject:@(0)];
+                        [g.AwayLB3Stats addObject:@(0)];
+                        
+                        [g.HomeCB1Stats addObject:@(0)];
+                        [g.AwayCB1Stats addObject:@(0)];
+                        
+                        [g.HomeCB2Stats addObject:@(0)];
+                        [g.AwayCB2Stats addObject:@(0)];
+                        
+                        [g.HomeCB3Stats addObject:@(0)];
+                        [g.AwayCB3Stats addObject:@(0)];
+                        
+                        [g.HomeSStats addObject:@(0)];
+                        [g.AwaySStats addObject:@(0)];
+                    }
+                }
+            }
+            
+            // if all league players were calculated, then recalculate
+            if (oldLigue.currentWeek > 14 && (oldLigue.allLeaguePlayers != nil || oldLigue.allLeaguePlayers.count != 0)) {
+                [oldLigue refreshAllLeaguePlayers];
+            }
+            
+            // if all conf players were calculated, then recalculate
+            for (Conference *c in oldLigue.conferences) {
+                if (oldLigue.currentWeek > 14 && (c.allConferencePlayers != nil || c.allConferencePlayers.count != 0)) {
+                    [c refreshAllConferencePlayers];
+                }
+            }
+            
             oldLigue.leagueVersion = HB_CURRENT_APP_VERSION;
         }
+        
         
         dispatch_async(dispatch_get_main_queue(), ^{
             dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
