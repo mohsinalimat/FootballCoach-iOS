00c58d40e6b34ef36de6bfb342dd60288b4ab89d
Akshay Easwaran <akeaswaran@me.com>
2019-03-17 23:59:30 -0400

    replaced large text blocks on recruiting and transfers with simple tip views

--- harbaughsim16/RecruitingPeriodViewController.m
+++ harbaughsim16/RecruitingPeriodViewController.m
@@ -34,6 +34,7 @@
 #import "RMessage.h"
 #import "UIScrollView+EmptyDataSet.h"
 #import "ZGNavigationBarTitleViewController.h"
+#import "ZMJTipView.h"
 
 #ifdef DEBUG
 #   define NSLog(...) NSLog(__VA_ARGS__)
@@ -41,7 +42,13 @@
 #   define NSLog(...) (void)0
 #endif
 
-@interface RecruitingPeriodViewController () <DZNEmptyDataSetDelegate, DZNEmptyDataSetSource>
+#define FCTutorialRecruitSelect 1000
+#define FCTutorialAdvanceWeek 1001
+#define FCTutorialViewCurrentRoster 1002
+#define FCTutorialViewTeamNeeds 1003
+#define FCTutorialCloseRecruiting 1004
+
+@interface RecruitingPeriodViewController () <DZNEmptyDataSetDelegate, DZNEmptyDataSetSource, ZMJTipViewDelegate>
 {
     ScrollableSegmentedControl *positionSelectionControl;
     STPopupController *popupController;
@@ -80,6 +87,33 @@
 
 @implementation RecruitingPeriodViewController
 @synthesize signedRecruitRanks,progressedRecruits,recruitingPoints,usedRecruitingPoints,recruitActivities;
+
+//MARK: ZMJTipViewDelegate
+- (void)tipViewDidDimiss:(ZMJTipView *)tipView {
+    // show new tips based on last shown tipview
+    if (tipView.tag == FCTutorialRecruitSelect) {
+        ZMJTipView *editTip = [[ZMJTipView alloc] initWithText:@"Tap here to view your current roster and check where you need to bolster it." preferences:nil delegate:self];
+        editTip.tag = FCTutorialViewCurrentRoster;
+        [editTip showAnimated:YES forItem:self.toolbarItems[0] withinSuperview:self.navigationController.view];
+    } else if (tipView.tag == FCTutorialViewCurrentRoster) {
+        ZMJTipView *editTip = [[ZMJTipView alloc] initWithText:@"Tap here to view a list of your roster's current positional needs." preferences:nil delegate:self];
+        editTip.tag = FCTutorialViewTeamNeeds;
+        [editTip showAnimated:YES forItem:self.toolbarItems[2] withinSuperview:self.navigationController.view];
+    } else if (tipView.tag == FCTutorialViewTeamNeeds) {
+        ZMJTipView *editTip = [[ZMJTipView alloc] initWithText:@"Tap here to close the recruiting window. Note: if you select this option, your open roster spots will be filled with walk-ons, and you will advance to start of next season." preferences:nil delegate:self];
+        editTip.tag = FCTutorialCloseRecruiting;
+        [editTip showAnimated:YES forItem:self.navigationItem.leftBarButtonItem withinSuperview:self.navigationController.view];
+    } else if (tipView.tag == FCTutorialCloseRecruiting) {
+        ZMJTipView *editTip = [[ZMJTipView alloc] initWithText:@"Tap here to advance to the next week of the transfer period. Be warned: other teams may make offers to recruits you have contacted." preferences:nil delegate:self];
+        editTip.tag = FCTutorialAdvanceWeek;
+        [editTip showAnimated:YES forItem:self.navigationItem.rightBarButtonItem withinSuperview:self.navigationController.view];
+    }
+}
+
+- (void)tipViewDidSelected:(ZMJTipView *)tipView {
+    // do nothing
+}
+
 
 -(void)backgroundViewDidTap {
     [popupController dismiss];
@@ -880,21 +914,18 @@
     availLBs = [NSMutableArray array];
 
     [self generateRecruits];
-
-    //display tutorial alert on first launch
-    BOOL tutorialShown = [[NSUserDefaults standardUserDefaults] boolForKey:HB_RECRUITING_TUTORIAL_SHOWN];
-    if (!tutorialShown) {
-        [[NSUserDefaults standardUserDefaults] setBool:YES forKey:HB_RECRUITING_TUTORIAL_SHOWN];
-        [[NSUserDefaults standardUserDefaults] synchronize];
-        [self showTutorial];
-    }
 }
 
 -(void)showTutorial {
     //display intro screen
-    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Welcome to Recruiting Season, Coach!" message:[HBSharedUtils recruitingTutorialText] preferredStyle:UIAlertControllerStyleAlert];
-    [alertController addAction:[UIAlertAction actionWithTitle:@"Ok" style:UIAlertActionStyleCancel handler:nil]];
-    [self presentViewController:alertController animated:YES completion:nil];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.25 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        if (self->totalRecruits.count > 0) {
+            NSString *tipText = @"Tap on a recruit to view how you can interact with them to recruit them to your program.";
+            ZMJTipView *editTip = [[ZMJTipView alloc] initWithText:tipText preferences:nil delegate:self];
+            editTip.tag = FCTutorialRecruitSelect;
+            [editTip showAnimated:YES forView:[self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]] withinSuperview:self.tableView];
+        }
+    });
 }
 
 -(void)viewWillAppear:(BOOL)animated {
@@ -1265,6 +1296,14 @@
             [hud hideAnimated:YES];
             [self->positionSelectionControl setSelectedSegmentIndex:0];
             [self.tableView reloadData];
+            
+            //display tutorial alert on first launch
+            //    BOOL tutorialShown = [[NSUserDefaults standardUserDefaults] boolForKey:HB_RECRUITING_TUTORIAL_SHOWN];
+            //    if (!tutorialShown) {
+            [[NSUserDefaults standardUserDefaults] setBool:YES forKey:HB_RECRUITING_TUTORIAL_SHOWN];
+            [[NSUserDefaults standardUserDefaults] synchronize];
+            [self showTutorial];
+            //    }
         });
     });
 }
