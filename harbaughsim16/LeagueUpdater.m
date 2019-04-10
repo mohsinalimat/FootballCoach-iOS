319e980ae691b91a692d2e07f0e7787e8b06043b
Akshay Easwaran <akeaswaran@me.com>
2019-04-09 22:28:30 -0400

    adjusted 2.1 updater to only run the all-league refreshers if you are updating to 2.1.x

--- harbaughsim16/LeagueUpdater.m
+++ harbaughsim16/LeagueUpdater.m
@@ -360,7 +360,8 @@
                 NSLog(@"ROTY: %@", roty);
             }
             
-            if (![[self class] needsUpdateFromVersion:oldLigue.leagueVersion toVersion:@"3.0"]) {
+            // we can't run these until 3.0 because of HC changes, so if we need to update to 3.0 from here, avoid running these
+            if (![HB_APP_VERSION_CURRENT_MINOR_VERSION isEqualToString:@"3.0"]) {
                 // if all league players were calculated, then recalculate
                 if (oldLigue.currentWeek > 14 && (oldLigue.allLeaguePlayers != nil || oldLigue.allLeaguePlayers.count != 0)) {
                     [oldLigue refreshAllLeaguePlayers];
