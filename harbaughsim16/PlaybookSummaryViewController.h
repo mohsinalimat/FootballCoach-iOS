8244599dd50c69e5a259aca3609890a9c676fff0
Akshay Easwaran <akeaswaran@me.com>
2019-04-29 19:10:16 -0400

    Adding playbook summary to recruiting

---
+++ harbaughsim16/PlaybookSummaryViewController.h
@@ -0,0 +1,17 @@
+//
+//  PlaybookSummaryViewController.h
+//  harbaughsim16
+//
+//  Created by Akshay Easwaran on 4/28/19.
+//  Copyright © 2019 Akshay Easwaran. All rights reserved.
+//
+
+#import "FCTableViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PlaybookSummaryViewController : FCTableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
